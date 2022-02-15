ecfa77ceaf89d0f78c85abb2e3aea3686163db7c
veectro <veectro@gmail.com>
2022-02-15 01:38:38 +0100

    fix: recovery windows for secret to 0

--- infra/secret_manager.tf
+++ infra/secret_manager.tf
@@ -1,5 +1,6 @@
 resource "aws_secretsmanager_secret" "udacity_secret" {
-  name = "udacity_dend2_secret"
+  name = "udacity_dend_2_secret"
+  recovery_window_in_days = 0
 }
 
 
@@ -8,7 +9,9 @@ resource "aws_secretsmanager_secret_version" "udacity_secret_version" {
   secret_id     = aws_secretsmanager_secret.udacity_secret.id
   secret_string = jsonencode(
     {
-      REDSHIFT_END_POINT = module.redshift.redshift_cluster_endpoint
-      RESHIFT_PASSWORD   = module.redshift.redshift_cluster_password
+      REDSHIFT_ENDPOINT = module.redshift.redshift_cluster_endpoint
+      REDSHIFT_DATABASE   = module.redshift.redshift_cluster_database
+      REDSHIFT_USERNAME   = module.redshift.redshift_cluster_username
+      REDSHIFT_PASSWORD   = module.redshift.redshift_cluster_password
     })
 }
