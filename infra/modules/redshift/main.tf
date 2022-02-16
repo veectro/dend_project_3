493a4039c444fb199e2ee6458de7ff3d0c46d645
veectro <veectro@gmail.com>
2022-02-16 20:27:31 +0100

    chore: random password with min_numeric

--- infra/modules/redshift/main.tf
+++ infra/modules/redshift/main.tf
@@ -1,15 +1,17 @@
 resource "aws_redshift_cluster" "default" {
-  cluster_identifier = var.cluster_identifier
-  database_name      = var.db_name
-  master_username    = var.username
-  master_password    = random_password.password.result
-  node_type          = var.node_type
-  cluster_type       = var.cluster_type
+  cluster_identifier  = var.cluster_identifier
+  database_name       = var.db_name
+  master_username     = var.username
+  master_password     = random_password.password.result
+  node_type           = var.node_type
+  cluster_type        = var.cluster_type
   skip_final_snapshot = true
+  iam_roles           = var.iam_roles
 }
 
 resource "random_password" "password" {
   length           = 16
   special          = true
+  min_numeric      = 1
   override_special = "_%@"
 }
