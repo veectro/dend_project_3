a4be961ba3d555d8a47c9c565885c88cb3de3271
veectro <veectro@gmail.com>
2022-02-19 00:38:36 +0100

    chore: update cluster type to multi node

--- infra/udacity.tf
+++ infra/udacity.tf
@@ -4,5 +4,7 @@ module "redshift" {
   username = "dwhuser"
   node_type = "dc2.large"
   db_name = "dwh"
+  cluster_type = "multi-node"
+  number_of_nodes = 4
   iam_roles = [aws_iam_role.redshift_iam_role.arn]
 }
